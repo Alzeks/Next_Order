4d857f36158a01e4782006c9480d469c222c101c
Alzeks <89770121+Alzeks@users.noreply.github.com>
2023-10-17 00:30:35 -0700

    Update

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -17,8 +17,6 @@ async function getData(id: number) {
 const SingleProductPage = async ({ params: {id} }:{params: {id: number}} ) => {
  
   const [data] = await (getData(id)) as TProduct[];
-  // const productData: Promise<product> = getProduct(id); const usertData: Promise<user> = getUser()
-  // const [product, user] = await Promise.all([productData, usertData])
 
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center">
@@ -37,7 +35,7 @@ const SingleProductPage = async ({ params: {id} }:{params: {id: number}} ) => {
       <div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
         <h1 className="text-3xl font-bold uppercase xl:text-5xl">{data.title}</h1>
         <p>{data.desc}</p>
-        {/* <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/> */}
+        
         <Price product={data} />
       </div>
     </div>
