39021fbaf77797b4f87e7bfb9e9c57f61c62ad7f
Alzeks <89770121+Alzeks@users.noreply.github.com>
2023-10-18 07:05:41 -0700

    Update

--- src/app/api/auth/config.ts
+++ src/app/api/auth/config.ts
@@ -1,19 +1,13 @@
 
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
-//import bcrypt from 'bcrypt'
 import { signWithAccessToken } from "@/app/(auth)/jwt";
-//import { cookies } from 'next/headers'
-//import { NextRequest } from "next/server";
 import GoggleProvider from 'next-auth/providers/google'
 import GitHubProvider from 'next-auth/providers/github'
-import { GithubEmail } from "next-auth/providers/github";
 import type { AuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { User } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
-import { SupabaseClient } from "@supabase/supabase-js";
-import { supabase } from "@/db/supabase/connect";
 
 declare module 'next-auth' {
   interface Session {
@@ -29,8 +23,6 @@ declare module 'next-auth/jwt' {
 
 export const authConfig: AuthOptions
   = {
-    //adapter:Supabase,//?
-    //session:{strategy:'jwt'},
   providers: [
     Credentials({
       credentials: { email: { label: "Username", type: "text", placeholder: "jsmith" }, password: { label: "Password", type: "password" } },
@@ -47,7 +39,7 @@ export const authConfig: AuthOptions
         if (!res.ok) { throw new Error("Failed to fetch data"); }
 
         const [user] = await res.json()
-        //const user = {email: 'email', name: 'Alex', role: 'admin'}//await getData()
+        
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
           const token = await signWithAccessToken({ username: user.username }); user.accessToken = token
@@ -67,32 +59,19 @@ export const authConfig: AuthOptions
       clientSecret: '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',
     }),
     GitHubProvider({
-      // clientId: HITHUB_ID,
-      // clientSecret: HITHUB_SECRET
-      clientId: '58237318493855b230dc',
-      clientSecret: '3114602b3d50d7c9af8d5a3a4450304f8849e7f3'
+       clientId: HITHUB_ID,
+       clientSecret: HITHUB_SECRET
     }),
   ],
   pages: { signIn: '/login' },//castom loginpage
   callbacks: {
     async jwt({ token, account, profile, user }) {
-      console.log('token', token, 'user', user, 'ac', account, profile);
-      //if(user)return {...token, token: user?.accessToken, role: user?.role, id: user.id }
-      //   if(user){token.user_id = user.id,//}//,
-      //   token.accessToken = user?.accessToken, token.role= user?.role}
       if (user) return { ...token, ...user }
       return token
     },
     async session({ session, token, user }) {
-      console.log('session', session, '2token', token, 'user', user);
-      // session.user.role = token.role,
-      // session.user.accessToken = token.accessToken,
-      //if(token){
-      session.user = token as any     //}
-      console.log('5', session);
+      session.user = token as any  
       return session
-      //return {...session, user: {...session.user, token: token.accessToken, role: token.role}}
     },
   },
-  //session: {strategy: 'jwt'},
 }
